1cb4741f065452bc1c33ccccdd78db8bc30fd6a3
Jeffrey C. Ollie <jeff@ocjtech.us>
2022-04-26 23:13:16 -0500

    nix

---
+++ default.nix
@@ -0,0 +1,12 @@
+with (import <nixpkgs> {});
+let env = bundlerEnv {
+    name = "jeffs-stuff";
+    inherit ruby;
+    gemfile = ./Gemfile;
+    lockfile = ./Gemfile.lock;
+    gemset = ./gemset.nix;
+  };
+in stdenv.mkDerivation {
+  name = "jeffs-stuff";
+  buildInputs = [env bundler ruby];
+}
