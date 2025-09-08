40a3a3f8df6f7d6a129678b9ed3e6699deaaa7fe
Jeffrey C. Ollie <jeff@ocjtech.us>
2025-09-08 14:56:14 -0500

    update

--- assets/copy-button.js
+++ assets/copy-button.js
@@ -2,22 +2,41 @@ let blocks = document.querySelectorAll("pre:has(code)");
 let copyButtonLabel = "Copy";
 
 blocks.forEach((block) => {
+  block.classList.add("code-pre");
+
   // Add a copy button to all blocks.
-  let div = document.createElement("div");
-  div.innerText = "Code";
+  let code = block.querySelector("code");
+  code.classList.add("code-content");
+  let code_content_wrapper = document.createElement("div");
+  code_content_wrapper.classList.add("code-content-wrapper");
+  code_content_wrapper.append(code);
+
+  let wrapper = document.createElement("div");
+  wrapper.classList.add("code-wrapper");
+
+  let header_outer = document.createElement("div");
+  header_outer.classList.add("code-header-outer");
+  let header_inner = document.createElement("div");
+  header_inner.classList.add("code-header-inner");
+  header_inner.innerText = "Code";
+
   let button = document.createElement("button");
   button.innerText = copyButtonLabel;
-  div.append(button);
-  block.prepend(div);
+
+  header_inner.append(button);
+  header_outer.append(header_inner);
+  wrapper.append(header_outer);
+  wrapper.append(code_content_wrapper);
+
+  block.prepend(wrapper);
 
   // handle click event
   button.addEventListener("click", async () => {
-    await copyCode(block, button);
+    await copyCode(code, button);
   });
 });
 
-async function copyCode(block, button) {
-  let code = block.querySelector("code");
+async function copyCode(code, button) {
   let text = code.innerText;
 
   await navigator.clipboard.writeText(text);
@@ -27,5 +46,5 @@ async function copyCode(block, button) {
 
   setTimeout(() => {
     button.innerText = copyButtonLabel;
-  }, 700);
+  }, 750);
 }
--- flake.nix
+++ flake.nix
@@ -42,12 +42,47 @@
       });
       packages = forAllSystems (pkgs: {
         adwaita-fonts = pkgs.callPackage ./adwaita.nix { };
+        american-typewriter-fonts = pkgs.callPackage ./american-typewriter.nix { };
       });
       apps = forAllSystems (pkgs: {
         release =
           let
             program = pkgs.writeShellScript "build-website" ''
               ${pkgs.lib.getExe zine.packages.${pkgs.system}.zine} release
+            '';
+          in
+          {
+            type = "app";
+            program = "${program}";
+          };
+        generate-favicon =
+          let
+            program = pkgs.writeShellScript "generate-favicon" ''
+              ${pkgs.imagemagickBig}/bin/magick \
+                assets/rush-starman.png \
+                -bordercolor white -border 0 \
+                  \( -clone 0 -resize 16x16 \) \
+                  \( -clone 0 -resize 24x24 \) \
+                  \( -clone 0 -resize 32x32 \) \
+                  \( -clone 0 -resize 48x48 \) \
+                  \( -clone 0 -resize 57x57 \) \
+                  \( -clone 0 -resize 64x64 \) \
+                  \( -clone 0 -resize 72x72 \) \
+                  \( -clone 0 -resize 76x76 \) \
+                  \( -clone 0 -resize 96x96 \) \
+                  \( -clone 0 -resize 110x110 \) \
+                  \( -clone 0 -resize 114x114 \) \
+                  \( -clone 0 -resize 120x120 \) \
+                  \( -clone 0 -resize 128x128 \) \
+                  \( -clone 0 -resize 144x144 \) \
+                  \( -clone 0 -resize 152x152 \) \
+                  \( -clone 0 -resize 180x180 \) \
+                  \( -clone 0 -resize 195x195 \) \
+                  \( -clone 0 -resize 196x196 \) \
+                  \( -clone 0 -resize 228x228 \) \
+                  \( -clone 0 -resize 270x270 \) \
+                  \( -clone 0 -resize 558x558 \) \
+                  -delete 0 -alpha off -colors 256 assets/favicon.ico
             '';
           in
           {
